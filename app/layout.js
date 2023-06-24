8dba3e8aa1b9aca46f4ca7c88e5e3de988d1856d
Liam <95655127+not-my-username@users.noreply.github.com>
2023-06-24 13:49:49 +1200

    add content

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " dark flex flex-col justify-between min-h-screen"}>{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'black-50%': '#18181bf2',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+      require('flowbite/plugin')
+  ],
+  darkMode: 'class',
+
 }
