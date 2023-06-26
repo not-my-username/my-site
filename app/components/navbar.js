cfff8ce14e1339aa6eb7c200dabfba2965618996
Liam <95655127+not-my-username@users.noreply.github.com>
2023-06-26 14:52:06 +1200

    hacker effect

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,8 +1,47 @@
+"use client"
+
+import { useEffect } from "react";
+
 export default function Navbar(user, current_page) {
+
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$%&";
+
+    
+
+    useEffect(() => {
+        
+        function regen(element) {
+            let iteration = 0;
+            let interval = null;
+            clearInterval(interval);
+    
+            interval = setInterval(() => {
+                element.innerText = element.innerText.split("").map((letter, index) => {
+                    if (index < iteration) {
+                        return element.getAttribute("value")[index];
+                    }
+                        return letters[Math.floor(Math.random() * 56)]
+                    }).join("");
+    
+                if (iteration >= element.getAttribute("value").length) {
+                    clearInterval(interval);
+                }
+    
+                iteration += 1 / 3;
+            }, 30);
+        }
+        const handleHover = (event) => {
+            regen(event.target)
+        };
+        document.getElementById("home-button").addEventListener("mouseenter", handleHover);
+        document.getElementById("about-button").addEventListener("mouseenter", handleHover);
+        document.getElementById("contact-button").addEventListener("mouseenter", handleHover);
+    }, []);
+
     return (
         <nav class="bg-gradient-to-b from-zinc-900 fixed w-full z-20 top-0 left-0 h-32">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
-                
+
                 <div class="flex md:order-2">
                     <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
@@ -12,16 +51,13 @@ export default function Navbar(user, current_page) {
                 <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                     <ul class="flex flex-col font-medium p-1 px-5 mt-4 border border-gray-100 rounded-2xl bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-zinc-900 dark:border-gray-700">
                         <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+                            <a href="#" class="block py-2 pl-3 pr-4 w-20 text-center text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 font-mono" aria-current="page" id="home-button" value="Home">Home</a>
                         </li>
                         <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                            <a href="#" class="block py-2 pl-3 pr-4 w-20 text-center text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-mono" id="about-button" value="Value">About</a>
                         </li>
                         <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                            <a href="#" class="block py-2 pl-3 pr-4 w-20 text-center text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-mono" id="contact-button" value="Contact">Contact</a>
                         </li>
                     </ul>
                 </div>
--- app/page.js
+++ app/page.js
@@ -26,7 +26,6 @@ export default function Home() {
   useEffect(() => {
     var title = document.getElementById('title');
     const orignalText = "Hi there!        I'm Liam"
-    console.log(title);
     setTimeout(() => {type(orignalText, title)}, 1000)
   }, []);
 
@@ -68,7 +67,7 @@ export default function Home() {
           <h1 id="title" class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white inline-block transition-all glow"></h1> {showElement && <p id="cursor" className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white flash inline-block">|</p>}
           <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">And I'm a student who's passionate about programming, filmmaking, and VFX. </p>
 
-          <a href="https://binary.liamsherwin.tech" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800 glow-blue">
+          <a href="https://binary.liamsherwin.tech" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800 shadow-lg shadow-blue-900/50 hover:shadow-blue-800/50 transition">
             <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Learn binary site! Check it out</span>
             <svg aria-hidden="true" class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
           </a>
