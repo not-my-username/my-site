94df539e5a715c81170af64d99355a0991143a72
Liam Sherwin <something.random.i.made.at.3am@gmail.com>
2023-11-20 21:33:31 +1300

    Fixed on mobile

--- pages/components/myWork.js
+++ pages/components/myWork.js
@@ -9,8 +9,8 @@ export default function MyWork() {
                     {/* <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here is a list of all the things I have made</p> */}
                 </div>
                 <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-                    <a class="bg-zinc-900 p-3 rounded-xl" href="https://spectrum.liamsherwin.tech">
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <a class="bg-zinc-900 m-3" href="https://spectrum.liamsherwin.tech">
+                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 ">
                             <span class="material-symbols-outlined">
                                 lightbulb
                             </span>
@@ -18,8 +18,8 @@ export default function MyWork() {
                         <h3 class="mb-2 text-xl font-bold dark:text-white">Spectrum</h3>
                         <p class="text-gray-500 dark:text-gray-400">Spectrum is a program to visulise music using stage lights for live shows, done in realtime</p>
                     </a>
-                    <a class=" bg-zinc-900 p-3 rounded-xl" href="https://rtt.liamsherwin.tech">
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <a class=" bg-zinc-900 m-3" href="https://rtt.liamsherwin.tech">
+                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
                             <span class="material-symbols-outlined">
                                 timer
                             </span>
@@ -27,8 +27,8 @@ export default function MyWork() {
                         <h3 class="mb-2 text-xl font-bold dark:text-white">Reaction Time Test</h3>
                         <p class="text-gray-500 dark:text-gray-400">A very very simple website to test your reaction time</p>
                     </a>
-                    <a class=" bg-zinc-900 p-3 rounded-xl" href="https://binary.liamsherwin.tech">
-                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                    <a class=" bg-zinc-900 m-3" href="https://binary.liamsherwin.tech">
+                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 ">
                             <span class="material-symbols-outlined">
                                 timer_10
                             </span>
