ee8eabd4a0872d62c6df6c4cbef738966b3d3f9f
Liam <someaccountmaybe@proton.me>
2025-11-02 23:10:11 +1300

    Updated Icon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,19 @@
 import "../styles/global.css";
+import Head from 'next/head'
+import TitleInfo from "../data/TitleInfo.json";
 
 export default function MySite({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+		<Head>
+			<title>{TitleInfo.title}</title>
+			<meta
+       			name="description"
+				content={TitleInfo.subheadding}
+				key="desc"
+			/>
+		</Head>
+		<Component {...pageProps} />;
+		</>
+	)
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,12 +8,12 @@ export default function Home() {
 		<>
 			<ScrollGridBG/>
 			<main className="dark">
-				<Navbar></Navbar>
-				<Title></Title>
-				<MyWork></MyWork>
+				<Navbar/>
+				<Title/>
+				<MyWork/>
 
 				<div class="h-80"></div>
-				<footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-gradient-to-t from-zinc-900 h-32 pointer-events-none"></footer>
+				<footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-gradient-to-t from-zinc-900 h-32 pointer-events-none text-center"></footer>
 			</main>
 		</>
 	);
